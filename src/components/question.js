472ff72eb18bffa6c7526ed797c6385836fd507d
Aliaksei Stelmakh <alexeistel@gmail.com>
2020-07-28 12:58:02 +0300

    feat: add questionBlock

---
+++ src/components/question.js
@@ -0,0 +1,27 @@
+import React from 'react';
+import myImage from '../assets/image/bird.jpg';
+
+export default function Question() {
+    return (
+        <div className="question">
+            <div className="question__block">
+                <ul className="question__list">
+                    <li className="question__item">Разминка</li>
+                    <li className="question__item">Воробьиные</li>
+                    <li className="question__item">Лесные птицы</li>
+                    <li className="question__item">Певчие птицы</li>
+                    <li className="question__item">Хищные птицы</li>
+                    <li className="question__item">Морские птицы</li>
+                </ul>
+            </div>
+            <div className="question__ans">
+                 <img className="question__image" src={myImage} alt="bird" />
+                <div className="bird-sound">
+                    <p className="question__bird">******</p>
+                    <hr />
+                    <audio className="question__sound" /* src={myAudio} */ controls />
+                </div>
+            </div>
+        </div>
+    )
+}
